8bcfa2ddf9d79fbd0817d895f1af3ffebc196a5f
sema abdelnasser <72020692+sema-byte@users.noreply.github.com>
2025-05-28 17:57:40 +0300

    updated

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,10 +1,5 @@
 // api.ts
-// const API_BASE_URL = 'http://localhost:5000/api';
-const API_BASE_URL = window.location.hostname === 'localhost' 
-  ? 'http://localhost:5000/api' 
-  : 'https://ecopal-production-2d9d.up.railway.app/api';
-
-
+const API_BASE_URL = 'http://localhost:5000/api';
 
 export interface ChatMessage {
   role: 'user' | 'assistant';
