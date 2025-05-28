dd0c0b4ca8e5a1b74ac00b20bd8c65d55cfc9382
sema abdelnasser <72020692+sema-byte@users.noreply.github.com>
2025-05-28 17:09:21 +0300

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,7 @@
 // const API_BASE_URL = 'http://localhost:5000/api';
 const API_BASE_URL = window.location.hostname === 'localhost' 
   ? 'http://localhost:5000/api' 
-  : 'https://ecopal-1eb9brxek-sema-bytes-projects.vercel.app/api';
+  : 'https://ecopal-pbl4mzm7u-sema-bytes-projects.vercel.app/api'; // Fix this URL!
 
 
 
